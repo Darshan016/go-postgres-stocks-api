8802238e517eab334be819854346063ec48ec766
darshan panchal <panchaldarshan121@gmail.com>
2024-06-16 16:20:42 +0530

    routes for the APIs

---
+++ router/router.go
@@ -0,0 +1,17 @@
+package router
+
+import (
+	"go-postgres-stocksAPI/middleware"
+
+	"github.com/gorilla/mux"
+)
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/api/v1/stocks", middleware.GetStocks).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/v1/stocks/{id}", middleware.GetStock).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/v1/stocks/", middleware.CreateStock).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/v1/stocks/{id}", middleware.UpdateStock).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/v1/stocks/{id}", middleware.DeleteStock).Methods("DELETE", "OPTIONS")
+	return router
+}
